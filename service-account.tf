1b183b169ea85f9c12d3cef2f7c262117479ad1d
Adminis <anoopdevopseng@gmail.com>
2025-07-15 20:38:21 +0530

    Update Module

--- service-account.tf
+++ service-account.tf
@@ -1,8 +1,8 @@
 #### Setup ServiceAccount
 resource "google_service_account" "cluster_service_account" {
   project      = var.project_id
-  account_id   = "${var.name}-sa"
-  display_name = "Terraform-managed service account for cluster ${var.name}"
+  account_id   = "${local.name}-sa"
+  display_name = "Terraform-managed service account for cluster ${local.name}"
 }
 
 resource "google_project_iam_member" "cluster_sa_permission" {
